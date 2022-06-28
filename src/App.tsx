27387ac4c884c24061be50eb3bebb9ad04f6754d
ktsudikov <ktsudikov@conceptsoft.io>
2022-06-28 15:23:32 +0300

    update demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import './App.css';
-import { Toggle, Button, Checkbox, IsometricMenu, ProfileCard} from 'ui-kir-on';
+import {Toggle, Button, Checkbox, ProfileCard, ProductCard, IsometricMenu} from 'ui-kir-on';
 
 function App() {
     const menuItems = [
@@ -53,8 +53,29 @@ function App() {
         ]
     }
     const [state, setState] = useState(false)
+    const [active, setActive] = useState('')
+    const productCardProps = {
+        imageSrc: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTvnBNwIx0kxHb4fWhQb379uCW2lwIG-2Wf7w&usqp=CAU',
+        title: 'Air trainer 3',
+        subTitle:'Women\'s shoe',
+        description: 'fkmsldf mlkdsmf lfsmd lfms ldmfls mdl mslsdf ds fsdl ,;flds, ;f,s',
+        param: {
+            name: 'Size',
+            valuesArray: ['36','38','40','42','44'],
+            active,
+            setActive,
+        },
+        price: 199.99,
+        onClick: () => console.log('Buy')
+    }
     return (
         <>
+            <div className="block" id="ProductCard">
+                <IsometricMenu menuItems={menuItems}/>
+                <div className="flexCenter">
+                    <ProductCard {...productCardProps} />
+                </div>
+            </div>
             <div className="block" id="ProfileCard">
                 <IsometricMenu menuItems={menuItems}/>
                 <div className="flexCenter">
